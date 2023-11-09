e1f4010ae57c610b8f198012a4b94d9c662c7bb7
Salleeem <mateusonego@gmail.com>
2023-11-09 19:29:30 -0300

    Cont

--- PetShop/Script.js
+++ PetShop/Script.js
@@ -1,4 +1,9 @@
-let icon = document.querySelector('ion-icon');
-icon.onclick = function(){
-  icon.classList.toggle('active');
-}
+document.addEventListener('DOMContentLoaded', function () {
+  let icons = document.querySelectorAll('ion-icon');
+  
+  icons.forEach(function(icon) {
+    icon.onclick = function() {
+      icon.classList.toggle('active');
+    }
+  });
+});
